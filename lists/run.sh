8e937d2976e28e65dc0d42144e7f2c8be903dcab
Anton Chelushkin <anton.chel@gmail.com>
2017-10-18 13:12:43 +0300

    Added ext. *.o to elf's

--- lists/run.sh
+++ lists/run.sh
@@ -4,7 +4,7 @@ if [[ $1 == "-c" ]];
 then 
     mkdir coverage && cd coverage
     cp ../lists.c lists.c
-    gcc --coverage lists.c -o lists
+    gcc --coverage lists.c -o lists.o
     python ../tester.py ../tests ./lists
     gcov lists.c
     lcov --directory ./ --capture --output-file tests.info --rc lcov_branch_coverage=1
@@ -15,18 +15,18 @@ then
 fi
 
 
-gcc lists.c -o lists -O2 -Wall -Werror -pedantic-errors -Wno-pointer-sign -Wextra -std=gnu11 -ftrapv -fsanitize=undefined
+gcc lists.c -o lists.o -O2 -Wall -Werror -pedantic-errors -Wno-pointer-sign -Wextra -std=gnu11 -ftrapv -fsanitize=undefined
 
 if [[ $? -eq 0 ]];
 then
     if [[ $1 == "" ]];
     then
-        ./lists
+        ./lists.o
     elif [[ $1 == "-v" ]];
     then
-        valgrind --leak-check=full ./lists 2>$2
+        valgrind --leak-check=full ./lists.o 2>$2
     elif [[ $1 == "-t" ]];
     then
-        python tester.py tests ./lists
+        python tester.py tests ./lists.o
     fi  
 fi
